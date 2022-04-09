da1707d0dce633e4858af33d5137d61937b70748
Nayeem-Bhuiyan <nayeembhuiyan40@gmail.com>
2022-04-09 15:53:40 +0600

    cvbcvb

--- App/Form1.Designer.cs
+++ App/Form1.Designer.cs
@@ -46,9 +46,9 @@
             // 
             this.button1.Location = new System.Drawing.Point(871, 73);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(104, 23);
+            this.button1.Size = new System.Drawing.Size(138, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Browse";
+            this.button1.Text = "Raw Folder Browse";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -106,9 +106,9 @@
             // 
             this.btnBrowseReference.Location = new System.Drawing.Point(899, 334);
             this.btnBrowseReference.Name = "btnBrowseReference";
-            this.btnBrowseReference.Size = new System.Drawing.Size(104, 23);
+            this.btnBrowseReference.Size = new System.Drawing.Size(127, 23);
             this.btnBrowseReference.TabIndex = 10;
-            this.btnBrowseReference.Text = "Browse";
+            this.btnBrowseReference.Text = "Ref Folder Browse";
             this.btnBrowseReference.UseVisualStyleBackColor = true;
             this.btnBrowseReference.Click += new System.EventHandler(this.btnBrowseReference_Click);
             // 
@@ -151,7 +151,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1018, 450);
+            this.ClientSize = new System.Drawing.Size(1100, 450);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.cmbCodeLength);
             this.Controls.Add(this.btnCreateRef);
